4d1f2a78d0fa85268492e198fb208c99052274f9
jose <jose@ThinkpadE15.>
2024-11-18 08:49:28 -0500

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+            <h1 className="text-white text-4xl font-bold">¡Hola desde React + Tailwind CSS!</h1>
+        </div>
+    );
+}
+
+export default App;
